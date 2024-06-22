0801c5bbc6371170f65229f44686e53e8c09fc15
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2024-06-22 18:59:20 +0100

    Created TicketBooth Version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <div className="app-container h-screen center">
+      <div className="w-[90%] text-justify">
+        <p className="font-semibold mb-[1rem]">Nenkemun,</p>
+        <p className="mb-[0.5rem]">A life without you is a life that I am grossly incapable of painting. Every single day, I earnestly wish and pray that you become mine.</p>
+        <p className="mb-[0.5rem]">To be honest; whenever I look at you, I see a snapshot of what the rest of my life promises to be and I want to get to that point faster than the speed light.</p>
+        <p className="mb-[0.5rem]">I am convinced that somehow somewhere in some universe out there Nenkemun also means <span className="italic">"Extreme Beauty"</span> because you are extremely beautiful. Regardless of the angle that I look at you from, your gorgeousness and charms never fail to mesmerize me.</p>
+        <p className="mb-[0.5rem]">Not only are you the most beautiful woman that I have had the honour to work with, you are also the greatest Work of Art that I have been priviledge to behold and experience.</p>
+        <p className="mb-[2rem]">Naekkeo, I love you so much and I would not hesitate to let you know at every chance that I get.</p>
+        <p><span className="text-[red]">&#x2665;</span>,</p>
+        <p>O.A Akinsoyinu</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
