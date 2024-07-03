50a8a964b0a6e07fa17bd5185fa1be825e6cc544
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2024-07-03 17:27:56 +0100

    Feat: Added More Homepage Content

--- src/components/HomeHeroSection.jsx
+++ src/components/HomeHeroSection.jsx
@@ -3,7 +3,7 @@ import HeroSearchBar from "./HeroSearchBar";
 
 const HomeHeroSection = () => {
     return (
-        <div className="home-hero-section h-[100dvh] bg-cover bg-center">
+        <div className="home-hero-section h-[100dvh] bg-cover bg-center pt-[5rem] lg:pt-[8rem]">
             <section className="center text-white text-center px-[1.6rem] lg:px-[8.8rem] h-[calc(100dvh-5rem)] lg:h-[calc(100dvh-8rem)]">
                 <section className="center">
                     <h1 className="font-robotoSerif mb-[1.6rem] lg:mb-[2.4rem] font-bold text-[2rem] lg:text-[4.8rem] leading-[2.342rem] lg:leading-[5.621rem]">Unlock Unforgettable Experiences with TicketDorm</h1>
