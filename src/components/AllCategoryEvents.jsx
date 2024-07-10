4632523d0db917f85e064e67bc9de3b3976fe925
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2024-07-10 17:53:22 +0100

    Fix: Altered Event Card Date Format

--- src/components/AllCategoryEvents.jsx
+++ src/components/AllCategoryEvents.jsx
@@ -5,7 +5,7 @@ const AllCategoryEvents = () => {
     return (
         <div className="pt-[5rem] lg:pt-[8rem] px-[1.6rem] lg:px-[8.8rem]">
             <Link to="/find-events" className="my-[1.5rem] block">
-            <section className="text-primaryPurple flex items-center font-montserrat"><FaArrowLeft className="w-[3rem]" /><span>Back to Find Events</span></section>
+            <section className="text-[rgba(14,72,135,1)] flex items-center font-montserrat"><FaArrowLeft className="w-[3rem]" /><span>Back to Find Events</span></section>
             </Link>
             <section>Map</section>
         </div>
