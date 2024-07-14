fb06ebf0abc3f4f65576691621b99c96ee5f3a23
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2024-07-14 09:04:39 +0100

    Feat: Added Event Page Functionality

--- src/utilis/EnsurePageLoadsFromTop.jsx
+++ src/utilis/EnsurePageLoadsFromTop.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { useLocation } from "react-router-dom";
+import AppContext from "../context/AppContext";
 
 const EnsurePageLoadsFromTop = props => {
-    const location = useLocation();
+    const { setShowLoadingAnimation } = useContext(AppContext),
+    location = useLocation();
 
     useEffect(() => {
+        setShowLoadingAnimation(false);
         window.scrollTo({top: 0, left: 0, behavior: "smooth"});
     }, [location]);
 
