978fb09ce56b6348ba4b91a45e6aeaf153742125
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2024-07-22 13:35:57 +0100

    Build: Added About Page Content and Changed Navigation List Items

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -12,7 +12,7 @@ const CheckoutPage = () => {
                 <title>{`${currentEvent.eventName} Checkout | TicketDorm`}</title>
                 </Helmet>
             <main>
-                <div className="pt-[5rem] lg:pt-[8rem] px-[1.6rem] lg:px-[8.8rem] font-inter">
+                <div className="pt-[5rem] lg:pt-[8rem] px-[1.6rem] lg:px-[8.8rem] font-montserrat">
                     <section className="mt-[1.6rem] lg:mt-[3.5rem] mb-[1.5rem]">
                         <h1 className="text-[2rem] lg:text-[4rem] font-bold mb-[0.5rem]">Let's Make Payment</h1>
                         <p className="text-[1.5rem] lg:text-[2.2rem]">Input your details to purchase a ticket, you will be redirected to a payment gateway once you are all set to pay.</p>
