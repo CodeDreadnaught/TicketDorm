7d03ad8bd5c4f1a9d9ed9ca2082f9e33ded89826
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2024-08-11 10:46:16 +0100

    Fix: Resolved Dashboard Stacking Order

--- src/components/DashboardHome.jsx
+++ src/components/DashboardHome.jsx
@@ -13,7 +13,7 @@ const DashboardHome = () => {
             <h1 className="font-semibold text-[1.8rem] lg:text-[2rem]">Hello, {user.firstname}.</h1>
             <p className="my-[1.5rem]">{eventsInformation.eventsMade > 0 ? "Here are the most recent events created by you;" : "Our records shows that you are yet to create an event, will you like to create one?"}</p>
             <section className="flex flex-col gap-[1rem]">
-                {eventsInformation.eventsMade > 0 ? recentEvents : <Link to="/sell-tickets" className="font-medium mt-[0.5rem]"><span className="border-b  border-[#007BFF] pb-[0.5rem] inline-block animate-bounce">Yes, I want to create an event</span></Link>}
+                {eventsInformation.eventsMade > 0 ? recentEvents : <Link to="/sell-tickets" className="font-medium mt-[0.5rem]"><span className="border-b  border-[#007BFF] pb-[0.5rem] inline-block animate-bounce relative z-[-1]">Yes, I want to create an event</span></Link>}
             </section>
         </section>
     );
